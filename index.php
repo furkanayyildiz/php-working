0d5b6825bc009e9326722f2b4a06cec4632ef1fc
Furkan Ayyıldız <furkan199905@gmail.com>
2021-11-29 19:25:44 +0300

    indexed and assoiative array examples

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php 
 
-	// echo 'hello, ninjas';
+    // indexed arrays
+	$peopleOne = ["Ahmet","Ali","Veli"];
+    $peopleTwo = array("Burcu","Buse");
+
+    //echo $peopleOne[1]; // printing index of array
+    $ages = [10, 20, 30, 40];
+    $ages[1] = 25; // changing element value
+
+    //adding new element which is exting array
+    $ages[] = 50;
+    array_push($ages, 60);
+    //print_r($ages);
+    //echo count($ages);
+
+    //sorting 2 array into array
+    $peopleThree = array_merge($peopleOne, $peopleTwo);
+    //print_r($peopleThree);
+
+    // Associative array (key & value pairs)
+
+    $fruitOne = [ "apple" => "red", "orange" => "orange", "kiwi" => "green"];
+    $fruitTwo = array("lemon" => "yellow", "strawberry" => "pink");
+    echo $fruitOne["kiwi"];
+    print_r($fruitTwo);
 
 ?>
 
@@ -10,6 +33,6 @@
 	<title>my first PHP file</title>
 </head>
 <body>
-	<h1><?php echo 'hello, ninjas' ?></h1>
+	
 </body>
 </html>
